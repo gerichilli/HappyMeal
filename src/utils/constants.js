5d1634677328bf2bc7abe24e2fbc3b92d7676b8a
gerichilli <hami.12206@gmail.com>
2022-12-12 18:10:53 +0700

    update site's url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const MAX_REVIEW_RECIPES = 9;
 export const MAX_RANDOM_RECIPES = 6;
 export const MAX_DROPDOWN_ITEMS = 5;
-export const BASE_URL = "http://localhost:8888";
+export const BASE_URL = "https://ourhappymeals.netlify.app";
 export const ITEMS_PER_PAGE = 16;
