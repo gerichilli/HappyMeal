4bf57ebf79d54d0c9bd981d9050fe8a49533357f
gerichilli <hami.12206@gmail.com>
2023-01-05 15:52:49 +0700

    fix firestore connection

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCOH_ImopHHRQEVEDoIvJPW8U9kCd-TVYQ",
+  authDomain: "porfolio-project-374af.firebaseapp.com",
+  projectId: "porfolio-project-374af",
+  storageBucket: "porfolio-project-374af.appspot.com",
+  messagingSenderId: "832084784559",
+  appId: "1:832084784559:web:f5e8eeb45b83c35fbc2b5a",
+  measurementId: "G-BK44P9CM28",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+  useFetchStreams: false,
+});
 export const storage = getStorage(app);
